63c204fc05f31a2aa8fcef213f49931b11d70e43
raulugarte <63106469+raulugarte@users.noreply.github.com>
2025-10-28 12:34:44 +0100

    Update AEM endpoints to new environment

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p137825-e1511252.adobeaemcloud.com';
+  return 'https://publish-p130407-e1279066.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p137825-e1511252.adobeaemcloud.com';
+  return 'https://author-p130407-e1279066.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
